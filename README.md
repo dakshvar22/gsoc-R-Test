b0ef40854b72fcaca49d7abf9d195e3249b2a059
Daksh <dakshvar22@gmail.com>
2016-03-21 19:17:06 +0530

    completed fourth

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ It takes as input -
 ## Test 3(test3.R)
 get_nouns is a R function to get nouns out of a tagged text input. This input should be an output of the earlier function - treeTagger_invoke.
 It takes only one input which is the tagged text.
+
+## Test 4(test4.R)
+As the test was to code any function which would be anyhow related to the project, I thought of coding up a function which would simply return the Document Term Matrix of a set of documents which would be fetched directly from a directory as a training Corpus. The function completely takes care of the standard data cleaning tasks like punctuation removal, stopword removal, etc. Such a function would obviously be the first step in a project related to supervised text mining and topic modelling. The function takes as input
+
+1) Path to directory containing documents used for training purposes
+
+As output it simply returns the DocumentTermMatrix Object.
+
+In addition to it, I also wrote a function which would also visualize the set of terms found in the training corpus in form of a word cloud. It takes as input the document term matrix got as output of the previous function and a mininmum threshold value on the frequency of each term.
---
+++ test4.R
@@ -0,0 +1,54 @@
+getDTM <- function(trainPath)
+{
+  ### Function to get the Document Term Matrix using the tm package. The function takes care of 
+  ### data cleaning steps such as punctuation removal and stopwords removal. 
+  
+  ### load required package - 'tm'
+  library(tm)
+  
+  ### Build a corpus of documents using the directory path given as function argument. Corpus()
+  ### function given by 'tm' package.
+  trainCorpus <- Corpus(DirSource(trainPath))
+  
+  ### Start with some data cleaning
+  
+  #create the toSpace content transformer from 'tm' package.
+  includeSpace <- content_transformer(function(x, pattern) {return (gsub(pattern, " " , x))})
+  trainCorpus <- tm_map(trainCorpus,includeSpace,"-")
+  trainCorpus <- tm_map(trainCorpus,includeSpace,":")
+  
+  #Replace Punctuation marks with " "
+  trainCorpus <- tm_map(trainCorpus,removePunctuation)
+  
+  ### Note: Inspect the corpus to find some special kind of punctuators like ' -' or " ' " which
+  ### need to be eliminated.
+  
+  #Transform to lower case.
+  trainCorpus <- tm_map(trainCorpus,content_transformer(tolower))
+  
+  #Remove english stop-words
+  trainCorpus <- tm_map(trainCorpus,removeWords,stopwords("english"))
+  
+  ### Stemming not handled as it has its own tradeoffs.
+  
+  ### FInal Step : Get Document-Term Matrix
+  mat <- DocumentTermMatrix(trainCorpus)
+  
+  ### Return DocumentTermMatrix object created by tm package
+  return(mat)
+}
+
+genWordCloud <- function(dtMat,minFreq)
+{
+  ### Function to generate a wordcloud of the terms found in the documents using the 'wordcloud' package.
+  ### Get term frequencies by summing over columns of the matrix
+  freq <- colSums(as.matrix(dtMat))
+  
+  ### load wordcloud package
+  library(wordcloud)
+  
+  #setting the same seed each time ensures consistent look across clouds
+  set.seed(42)
+  #limit words by specifying min frequency
+  wordcloud(names(freq),freq, min.freq=minFreq,colors=brewer.pal(6,"Dark2"))
+}
